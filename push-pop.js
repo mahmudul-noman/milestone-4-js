4cb1c8ffcb364227b2d3a9337c8d91009bba8eae
Mahmudul-H-Noman <mhnoman75@gmail.com>
2023-01-27 18:38:09 +0600

    added push-pop

---
+++ push-pop.js
@@ -0,0 +1,17 @@
+var numbers = [78, 88, 55, 54, 62];
+
+// ::::::: Push: Added new element in last position in array ::::::::::::
+// numbers.push(63);
+// console.log(numbers);
+var friends = ['Soton', 'Mizan', 'Mamun', 'Sajeeb'];
+friends.push('Niloy');
+// console.log(friends);
+var Added_First_Elements = friends.unshift('Rakib');
+// console.log(Added_First_Elements);
+console.log(friends);
+
+// :::::::: Pop: Remove last element in array :::::::::::::::::::::::::::
+var Remove_Last_Elements = friends.pop('');
+var Remove_First_Elements = friends.shift();
+// console.log(friends);
+// console.log(Remove_First_Elements);
