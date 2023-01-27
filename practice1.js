7ba8987d1323f49ae3e80028aa4e5183b149a5ce
Mahmudul-H-Noman <mhnoman75@gmail.com>
2023-01-27 12:26:24 +0600

    added practice module 1

---
+++ practice1.js
@@ -0,0 +1,34 @@
+//:::::::: PRACTICE PROBLEM 1 (Summation & Substraction):::::::::::::::
+// var Total_Money = 1000;
+// var buyOrange = 400;
+// var buyApple = 300;
+
+// var Total_Shopping = buyOrange + buyApple;
+// var moneyBalance = Total_Money - Total_Shopping;
+// console.log(moneyBalance);
+
+
+//::::::::: PRACTICE PROBLEM 2 (toFixed):::::::::::::::
+var math = 75.25;
+var biology = 65;
+var chemistry  = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var Total_Result = math + biology + chemistry + physics + bangla;
+var Average_Result = Total_Result / 5;
+console.log(Average_Result.toFixed(2));
+
+
+//::::::::: PRACTICE PROBLEM 3 (Concatentation):::::::::::::::
+// var firstLine = "I am going to be";
+// var lastLine  = "an awesome web developer";
+
+// var combindLine = firstLine + ' ' + lastLine;
+// console.log(combindLine);
+
+
+//::::::::: PRACTICE PROBLEM 4 (Remainder):::::::::::::::
+// var GivenNumber = 119;
+// var RemainderNumber = 119 % 5;
+// console.log(RemainderNumber);
---
+++ quiz.js
@@ -0,0 +1,8 @@
+// Q1
+// var age = 21;
+// age += 8;
+// console.log(age);
+
+
+// Q2
+console.log(17%5);
