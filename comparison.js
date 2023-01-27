52595c1156853f9e64e9c64c8764a2eb5c57a3ca
Mahmudul-H-Noman <mhnoman75@gmail.com>
2023-01-27 20:10:53 +0600

    added comaparison & Condition

---
+++ comparison.js
@@ -0,0 +1,36 @@
+// :::::::: LESS Then
+// console.log(5 < 6);
+
+// ::::: Greater Then
+// console.log(5 > 6);
+
+// :::::::::Not Equal
+// console.log(5 == 6);
+
+// ::::::: Equal
+// console.log (5 == 5);
+
+// ::::::: Not Equal
+// console.log(5 != 6);
+
+// :::::::Not Equal
+// console.log(5!=5);
+
+// :::::::Less then or equal
+// console.log(5 <= 6);
+
+// :::::::: Greater Than or Equal
+// console.log(5>=5);
+
+var num1 = 56;
+var num2 = 98;
+console.log(num1 < num2);
+console.log(num1 > num2);
+console.log(num1 <= num2);
+console.log(num1 >= num2);
+console.log(num1 !== num2);
+
+// ::::::::: Multiple Condition
+// money1 > money2 && result1 > result2 && height1 > height2
+money1 > money2 || result1 > result2 || height1 > height2
+
---
+++ conditional.js
@@ -0,0 +1,18 @@
+var iPhone__Price = 79000;
+var my__Budget = 9500;
+
+// if iPhone price is less then my budget. I will purchase this
+if (iPhone__Price <  my__Budget) {
+    console.log('Wow! Kine Pelechi');
+}
+else {
+    console.log('Budget Nai.')
+}
+
+// var chickenPrice = 500;
+// var myMoney = 50;
+// if (chickenPrice <= myMoney) {
+//     console.log('Purchase Done');
+// }
+// if iPhone price is grater then my budget. I will not purchase this
+
