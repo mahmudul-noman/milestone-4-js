8c193d55d4aaead5c87875d2a72e45f3d32a45cb
Mahmudul-H-Noman <mhnoman75@gmail.com>
2023-01-27 09:47:29 +0600

    added some js

---
+++ math.js
@@ -0,0 +1,16 @@
+/***
+ * 1. number ===> 55, 855, 19.2
+ * 2. integer, odd,even
+ * 3. float: decimal: 2.5, 32.5
+ * 4. Operation: addition, substraction, multiplication, division, reminder
+ * 5. Percent
+ * 6. average
+ * 7. Comparison: >, <, ==, !=, >=, <=
+ * 8. Oikik: per item prcie * total qty = ?
+ * 9. max, min, average, sum
+ * 10. series
+ * EXTRA
+ * 1. Prime
+ * 2. Permutation
+ * :::::::::::::::::
+*/
