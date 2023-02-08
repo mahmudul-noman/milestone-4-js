bacd9e9ddf98cd9f49f4126f6fe328800153fba6
Mahmudul-H-Noman <mhnoman75@gmail.com>
2023-02-09 01:06:19 +0600

    added final commit

--- quiz.js
+++ quiz.js
@@ -62,8 +62,75 @@
 
 
 
-let sum=0; 
-for( let i = 0; i<=3;i++){ 
-sum = sum + i; 
+// let sum=0; 
+// for( let i = 0; i<=3;i++){ 
+// sum = sum + i; 
+// }
+// console.log(sum);
+
+
+
+
+
+
+// function compare(a, b) {
+
+//     if (a == b) {
+   
+//     return true;
+   
+//     } else {
+   
+//     return false;
+   
+//     }
+   
+//    }
+   
+//    const result = compare(15, "15");
+   
+//    console.log(result);
+
+
+
+
+
+
+
+
+
+// function compare(a, b) {
+
+//     if (a.toString() === b) {
+   
+//     return true;
+   
+//     } else {
+   
+//     return false;
+   
+//     }
+   
+//    }
+   
+//    const result = compare(25, 25);
+   
+//    console.log(result);
+
+
+
+
+
+
+
+// let a="hi"
+
+// let b="there"
+
+// console.log(a+b)
+
+
+
+for(i=5; i<=100; i++){
+    console.log(i);
 }
-console.log(sum);
